3853a49f9ee139605465525cb42d8c10f3503baa
zdren <zdren@gigadevice.com>
2017-10-15 00:34:03 +0800

    update 1014-02

--- leaktest.cpp
+++ leaktest.cpp
@@ -17,8 +17,9 @@ leakTest::leakTest(QWidget *parent, Posix_QextSerialPort *serial) :
     pTimer->setInterval(10);
     connect(pTimer, SIGNAL(timeout()), this, SLOT(onTimeOut()));
 
-    ui->label_view->setText(QString("0.000000"));
+    ui->lineEdit_D_value->setText(QString("0.000000"));
     ui->lineEdit_time->setText(QString("0.00"));
+    ui->pushButton_2->hide();
     ui->pushButton_2->setEnabled(false);
 
     switch(g32styleMode)
@@ -77,9 +78,14 @@ void leakTest::onTimeOut(void)
 
         if(btimeStartFlag)
         {
+            ui->pushButton->setText(QString::fromUtf8("停止"));
             m_timer += 0.01;
             str = QString::number(m_timer, 'f', 2);
             ui->lineEdit_time->setText(str);
+        }
+        else
+        {
+            ui->pushButton->setText(QString::fromUtf8("开始"));
         }
     }
 }
@@ -88,6 +94,8 @@ void leakTest::on_pushButton_clicked()
 {
     QString strsms;
     QString str;
+    static u_int8_t clickSts = 0;
+    clickSts++;
     sendSerialCommand(pSerial, CMD_GET_PRESSURE_READING, &strsms);
 
     if(strsms.length() != 0)
@@ -100,23 +108,10 @@ void leakTest::on_pushButton_clicked()
         ui->label_view->setText(str);
     }
     m_timer = 0;
-    btimeStartFlag = true;
-    ui->pushButton->setEnabled(false);
-    ui->pushButton_2->setEnabled(true);
-    ui->pushButton->setStyleSheet(QString::fromUtf8("border-top-left-radius:6px;\n"
-"color: rgb(255, 255, 255);\n"
-"border:2px solid rgba(255, 255, 255,70);\n"
-"background-color: rgba(255, 255, 255, 0);\n"
-"border-top-right-radius:6px;\n"
-"border-bottom-left-radius:6px;\n"
-"border-bottom-right-radius:6px;"));
-    ui->pushButton_2->setStyleSheet(QString::fromUtf8("border-top-left-radius:6px;\n"
-"color: rgb(255, 255, 255);\n"
-"border:2px solid rgba(255, 255, 255,70);\n"
-"background-color: rgba(255, 255, 255, 20);\n"
-"border-top-right-radius:6px;\n"
-"border-bottom-left-radius:6px;\n"
-"border-bottom-right-radius:6px;"));
+    if(clickSts % 2)
+        btimeStartFlag = true;
+    else
+        btimeStartFlag = false;
 }
 
 void leakTest::on_pushButton_2_clicked()
@@ -142,7 +137,7 @@ void leakTest::on_pushButton_2_clicked()
 void leakTest::on_btn_configureMachine_2_clicked()
 {
     pTimer->stop();
-    ui->label_view->setText(QString("0.000000"));
+    ui->lineEdit_D_value->setText(QString("0.000000"));
     ui->lineEdit_time->setText(QString("0.00"));
     this->close();
 }
